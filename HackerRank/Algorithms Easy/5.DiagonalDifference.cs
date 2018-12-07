bec3567e1bf3b2be5fb7906340c2913f439693dc
erjan123 <erjan123@yahoo.com>
2018-12-07 20:33:10 +0200

    Diagonal Difference

---
+++ HackerRank/Algorithms Easy/5.DiagonalDifference.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+
+namespace HackerRank.Algorithms_Easy
+{
+    public static class DiagonalDifference
+    {
+        public static void DiagonalDifferenceMethod()
+        {
+            int[][] array = { new int[] { 1, 2, 3 },
+                              new int[] { 4, 5, 6 },
+                              new int[] { 9, 8, 9 }
+                            };
+
+            Console.Write("Diagonal Difference with two loops : ");
+            Console.WriteLine(diagonalDifference1(array));
+
+            Console.Write("Diagonal Difference with one loop : ");
+            Console.WriteLine(diagonalDifference2(array));
+        }
+        private static int diagonalDifference1(int[][] a)
+        {
+            int diaglonal1 = 0;
+            int diaglonal2 = 0;
+
+            int arrayCount = a.Count();
+
+            for (int i = 0; i < a.Count(); i++)
+            {
+                diaglonal1 += a[i][i];
+            }
+
+            int temp = 0;
+            for (int y = a.Count() - 1; y >= 0; y--)
+            {
+                diaglonal2 += a[temp][y];
+                temp++;
+            }
+
+            return Math.Abs(diaglonal1 - diaglonal2);
+
+        }
+
+        private static int diagonalDifference2(int[][] a)
+        {
+            int sum = 0;
+            int j = a.Length - 1;
+            for (int i = 0; i < a.Count(); i++)
+            {
+                sum  += a[i][i] - a[j][i];
+                j--;
+            }
+
+            return Math.Abs(sum);
+        }
+
+    }
+}
--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -9,18 +9,20 @@ namespace HackerRank
         static void Main(string[] args)
         {
             // 1. HackerRank - "Solve Me First" - https://www.hackerrank.com/challenges/solve-me-first/problem
-            //Console.WriteLine(SolveMeFirst.solveMeFirst(1, 2));
+            // Console.WriteLine(SolveMeFirst.solveMeFirst(1, 2));
 
             // 2. HackerRank - "Simple Array Sum" - https://www.hackerrank.com/challenges/simple-array-sum/problem
-            //Console.WriteLine(SimpleArraySum.simpleArraySum(new int[] { 1, 2, 3, 4, 5 }));
-            //Console.WriteLine(SimpleArraySum.simpleArraySum_WithLoop(Enumerable.Range(1, 5).ToArray()));
+            // Console.WriteLine(SimpleArraySum.simpleArraySum(new int[] { 1, 2, 3, 4, 5 }));
+            // Console.WriteLine(SimpleArraySum.simpleArraySum_WithLoop(Enumerable.Range(1, 5).ToArray()));
 
             // 3. HackerRank - "Compare The Triplets" - https://www.hackerrank.com/challenges/compare-the-triplets/problem
             // CompareTheTriplets.CompareTheTriplets1();
 
             // 4. HackerRank - "A Very Big Sum" - https://www.hackerrank.com/challenges/a-very-big-sum/problem
-            AVeryBigSum.AVeryBigSumMethod();
+            // AVeryBigSum.AVeryBigSumMethod();
 
+            // 5. HackerRank - "Diagonal Difference" - https://www.hackerrank.com/challenges/diagonal-difference/problem
+            DiagonalDifference.DiagonalDifferenceMethod();
 
             Console.Read();
         }
