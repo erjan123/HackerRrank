38d44187be4f1eaf51cb9782b10eb8ba722a984a
erjan123 <erjan123@yahoo.com>
2018-12-07 21:19:43 +0200

    Arrays DS

---
+++ HackerRank/Data Structures Easy/1.ArraysDS.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HackerRank.Data_Structures_Easy
+{
+    public static class ArraysDS
+    {
+        public static void ReverseArray()
+        {
+            int[] array = Enumerable.Range(1, 10).ToArray();
+
+            Console.Write("Reverse Array : ");
+            reverseArray(array).ToList().ForEach(x => Console.Write(x + " "));
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write("Reverse Array with Linq Reverse() method : ");
+            array.Reverse().ToList().ForEach(x => Console.Write(x + " "));
+        }
+        static int[] reverseArray(int[] a)
+        {
+            int[] temp = new int[a.Count()];
+            for (int i = 1; i <= a.Count(); i++)
+            {
+                temp[i - 1] = a[a.Count() - i];
+            }
+            return temp;
+        }
+    }
+}
--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using HackerRank.Algorithms_Easy;
 using System.Linq;
+using HackerRank.Data_Structures_Easy;
 
 namespace HackerRank
 {
@@ -8,21 +9,33 @@ namespace HackerRank
     {
         static void Main(string[] args)
         {
-            // 1. HackerRank - "Solve Me First" - https://www.hackerrank.com/challenges/solve-me-first/problem
+            #region Algorithms Easy
+
+            // 1. "Solve Me First" - https://www.hackerrank.com/challenges/solve-me-first/problem
             // Console.WriteLine(SolveMeFirst.solveMeFirst(1, 2));
 
-            // 2. HackerRank - "Simple Array Sum" - https://www.hackerrank.com/challenges/simple-array-sum/problem
+            // 2. "Simple Array Sum" - https://www.hackerrank.com/challenges/simple-array-sum/problem
             // Console.WriteLine(SimpleArraySum.simpleArraySum(new int[] { 1, 2, 3, 4, 5 }));
             // Console.WriteLine(SimpleArraySum.simpleArraySum_WithLoop(Enumerable.Range(1, 5).ToArray()));
 
-            // 3. HackerRank - "Compare The Triplets" - https://www.hackerrank.com/challenges/compare-the-triplets/problem
+            // 3. "Compare The Triplets" - https://www.hackerrank.com/challenges/compare-the-triplets/problem
             // CompareTheTriplets.CompareTheTriplets1();
 
-            // 4. HackerRank - "A Very Big Sum" - https://www.hackerrank.com/challenges/a-very-big-sum/problem
+            // 4. "A Very Big Sum" - https://www.hackerrank.com/challenges/a-very-big-sum/problem
             // AVeryBigSum.AVeryBigSumMethod();
 
-            // 5. HackerRank - "Diagonal Difference" - https://www.hackerrank.com/challenges/diagonal-difference/problem
-            DiagonalDifference.DiagonalDifferenceMethod();
+            // 5. "Diagonal Difference" - https://www.hackerrank.com/challenges/diagonal-difference/problem
+            // DiagonalDifference.DiagonalDifferenceMethod();
+
+            #endregion
+
+            #region Data Structures 
+
+            // 1. ArraysDS - https://www.hackerrank.com/challenges/arrays-ds/problem
+            ArraysDS.ReverseArray();
+
+
+            #endregion
 
             Console.Read();
         }
