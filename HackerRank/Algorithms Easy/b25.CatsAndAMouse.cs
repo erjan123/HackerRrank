2439f010845542a0bb8e9cd7c920a5993d07f68e
erjan123 <erjan123@yahoo.com>
2018-12-11 23:51:56 +0200

    CatsAndAMouse

---
+++ HackerRank/Algorithms Easy/b25.CatsAndAMouse.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace HackerRank.Algorithms_Easy
+{
+    public class CatsAndAMouse
+    {
+        public static void CatsAndAMouseMethod()
+        {
+            Console.WriteLine("Solution - Test 1");
+            Console.WriteLine(catAndMouse(1,2,3));
+
+            Console.WriteLine();
+            Console.WriteLine("Solution - Test 2");
+            Console.WriteLine(catAndMouse(1, 3, 2));
+        }
+        static string catAndMouse(int x, int y, int z)
+        {
+            int catA = Math.Abs(z - x);
+            int catB = Math.Abs(z - y);
+
+            if (catA < catB)
+            {
+                return "Cat A";
+            }
+            else if (catA > catB)
+            {
+                return "Cat B";
+            }
+            else if (catA == catB)
+            {
+                return "Mouse C";
+            }
+
+            return "";
+        }
+    }
+}
--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -86,6 +86,9 @@ namespace HackerRank
             // ElectronicsShop.GetMoneySpent();
 
             // 25. "Cats and a Mouse" - https://www.hackerrank.com/challenges/cats-and-a-mouse/problem
+            CatsAndAMouse.CatsAndAMouseMethod();
+
+
             // 26. "The Hurdle Race" - https://www.hackerrank.com/challenges/the-hurdle-race/problem
             // 27. "Designer PDF Viewer" - https://www.hackerrank.com/challenges/designer-pdf-viewer/problem
             // 28. "Utopian Tree" - https://www.hackerrank.com/challenges/utopian-tree/problem
