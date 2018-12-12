f9d8c2a1eb458029b9a1a435393e8c741c972210
erjan123 <erjan123@yahoo.com>
2018-12-12 10:52:47 +0200

    Repeated String

--- HackerRank/Algorithms Easy/31 to 60/b35.JumpingOnTheClouds.cs
+++ HackerRank/Algorithms Easy/31 to 60/b35.JumpingOnTheClouds.cs
@@ -1,10 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace HackerRank.Algorithms_Easy
 {
-    class JumpingOnTheClouds
+    public class JumpingOnTheClouds
     {
+        public static void JumpingOnTheCloudsMethod()
+        {
+            Console.WriteLine("Solution:");
+            Console.WriteLine(jumpingOnClouds(new int[] { 0, 0, 1, 0, 0, 1, 0 }));
+        }
+
+        static int jumpingOnClouds(int[] c)
+        {
+            int count = 0;
+
+            for (int i = 0; i < c.Count() - 1; i++)
+            {
+                if (i + 2 <= c.Length - 1 && c[i + 2] != 1)
+                {
+                    i += 1;
+                }
+
+                count++;
+                if (i > c.Count())
+                    break;
+            }
+            return count;
+        }
     }
 }
